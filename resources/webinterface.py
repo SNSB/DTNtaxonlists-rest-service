f791884d0548d3ab070c90627ac192832045e447
stefan seifert <seifert@bsm.mwn.de>
2015-04-07 10:30:32 +0200

    integrated whoosh search fixed references added some more web representations compiled a swagger service description.

---
+++ resources/webinterface.py
@@ -0,0 +1,37 @@
+# webinterface...
+
+import requests
+from urlparse import urlparse
+import urllib2
+
+from database.commonname import getcommonname
+from database.agent import getagents, getagent, getagentrelations
+from database.project import getproject, getprojectagents
+from database.name import *
+from database.list import getList, getAllLists, getListProject
+
+from resources.names import namecommonnames
+
+
+from flask.ext import restful
+from flask import Flask,g, request, render_template
+from flask import url_for
+from flask.ext.restful import Resource, fields, marshal_with
+from flask import url_for, Response
+
+class wwwtaxonlist(restful.Resource):
+    def get(self, database, id):
+        taxonnamelist = getList(database, id)
+        for nameitem in taxonnamelist:
+            nameinfo = getName(nameitem['database'], nameitem['nameid'])
+            commonnamesurl=url_for("namecommonnames", database=nameitem['database'], id=nameitem['nameid'], _external=True)
+            nameinfo[0]['commonnamesurl']=commonnamesurl
+            allcommonnames=getNameAllCommonNames(nameitem['database'], nameitem['nameid'])
+            nameinfo[0]['commonnames']=allcommonnames
+            nameitem['nameinfo']=nameinfo
+        taxonlistproject = getproject('DiversityProjects_TNT',id)
+        taxonlistagents = getprojectagents('DiversityProjects_TNT', id)
+        response = Response(render_template("taxonlist_internal.html", database=database, id=id, taxonnames=taxonnamelist, taxonlistproject=taxonlistproject, taxonlistagents=taxonlistagents) )
+        response.headers['content-type'] = 'text/html'
+        return response
+    
---
+++ searchengine/suchen.py
@@ -0,0 +1,83 @@
+ 
+# searchengien whoosh interface
+from whoosh.index import create_in
+import whoosh.index as index
+from whoosh.fields import *
+from whoosh.qparser import QueryParser, MultifieldParser
+from whoosh.query import FuzzyTerm, Every
+from whoosh import query, qparser
+
+import os, os.path
+from database.name import *
+from database.dbtaxonname import *
+from flask import url_for
+import six # unicode 
+
+from flask import Response, render_template
+
+
+def createindex():
+    # creates the index and deltes an existing one.
+    schema = Schema(name=TEXT(stored=True), # the latin name
+                    url=ID(stored=True), # the url of the name
+                    commonname=TEXT(stored=True), # the common names, comma separated
+                    project=TEXT(stored=True),
+                    database=TEXT, # in which database
+                    country=TEXT, # all countries, comma separated
+                    tags=KEYWORD)
+    if not os.path.exists("index"):
+       os.mkdir("index")
+    ix = create_in("index", schema)
+    writer = ix.writer()
+    allnames=getAllNames()
+    for item in allnames:
+        if item['ProjectID'] in [701, 704, 1137, 855, 1143, 1144, 849]:
+            uri = u"" + url_for('name', database=item['DatabaseName'], id = item['NameID'], _external=True)
+            commonnames=getTaxonNameAllCommonNames(item['DatabaseName'], item['NameID'])
+            cn = u" "
+            co = u" "
+            for n in commonnames:
+                cn += n['CommonName'] + u", "
+                co += n['CountryCode'] + u", "
+            if len(cn)>1:
+                cn=cn.strip()
+            namelist = getTaxonName(item['DatabaseName'], item['NameID'])
+            taxonname = u""
+            keys = u""
+            if len(namelist)>0:
+                taxonname = namelist[0]['TaxonNameCache']
+                keys = u",".join(filter(lambda l: isinstance(l, six.string_types), namelist[0].values()))
+            print("country: %s " % co)
+            print("commonname: %s " % cn)
+            writer.add_document(name=taxonname, url=uri, database=item['DatabaseName'], project=u"%s" % item['ProjectID'], commonname=cn, country=co, tags=keys)
+    #        else:
+    #          writer.add_document(name=taxonname, url=uri, database=item['DatabaseName'], tags=keys)
+    writer.commit()
+    
+def indexquery(name,www):
+    if name==None:
+        return []
+    ix = index.open_dir("index")
+    qp = MultifieldParser(["commonname", "database", "tags", "name"], schema=ix.schema, termclass=FuzzyTerm)
+    qp.add_plugin(qparser.FuzzyTermPlugin())
+    q = qp.parse(name)
+    #q = Every()
+    tempvar=[]
+    with ix.searcher() as searcher:
+        results = searcher.search(q, limit=None)
+        for hit in results:
+            tempvar.append({'name':hit["name"], 'commonname':hit["commonname"] , 'url':hit["url"]})
+    if  not www:
+        return tempvar
+    else:
+        response = Response(render_template("searchresults.html", resultlist=tempvar) )
+        response.headers['content-type'] = 'text/html'
+        return response
+       
+        
+    
+            
+        
+    
+
+
