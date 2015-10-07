068705756a31c2535538c80fc1c7b89858d7b022
stefan seifert <seifert@bsm.mwn.de>
2015-10-07 12:40:50 +0200

    zip export of list has now also meta.xml and eml.eml for darwin core. common-names are not exportet (no file in zip and no reference in meta.xml) if no common-names are available.

--- resources/taxonlist_flat_csv.py
+++ resources/taxonlist_flat_csv.py
@@ -7,7 +7,8 @@ import urllib2
 from resources.lists import taxonlistflat
 
 from database.dbtaxonname import getAllTaxonNamesFromListFlat, getTaxonNameAllAcceptedNames, getAllCommonNamesFromListFlat
-from database.dbprojects import getProject
+from database.dbprojects import getProject, getProjectAgents
+from database.dbagents import getAgent
 
 from flask.ext import restful
 from flask import Flask,g, request, render_template
@@ -19,6 +20,8 @@ class taxonlist_flat_csv(restful.Resource):
     def get(self, database, id):
         project = getProject('DiversityProjects_TNT', id)
         taxonnamelist = getAllTaxonNamesFromListFlat(database, id)
+        if len(taxonnamelist) <= 0:
+            restful.abort(404)
         for row in taxonnamelist:
             uri =  url_for('name', database=database, id = row['NameID'], _external=True)
             row['taxonID'] = uri
@@ -42,8 +45,13 @@ class taxonlist_flat_csv(restful.Resource):
 
 class   darwin_core_zip( restful.Resource ):
     def get(self, database, id):
+        
+        thisurl = url_for('darwin_core_zip', database=database, id = id, _external=True)
+        
         project = getProject('DiversityProjects_TNT', id)
         taxonnamelist = getAllTaxonNamesFromListFlat(database, id)
+        if len(taxonnamelist) <= 0:
+            restful.abort(404)
         for row in taxonnamelist:
             uri =  url_for('name', database=database, id = row['NameID'], _external=True)
             row['taxonID'] = uri
@@ -69,6 +77,27 @@ class   darwin_core_zip( restful.Resource ):
             uri =  url_for('name', database=database, id = row['NameID'], _external=True)
             row['taxonID'] = uri
         taxoncommonname_csv = render_template("taxonlistcommonnames_flat_csv.j2", database=database, id=id, commonnames=taxoncommonnamelist, project=project[0]) 
+        
+        projectagentlist = getProjectAgents('DiversityProjects_TNT', id)
+        for row in projectagentlist:
+            agenturi = urlparse(row['AgentURI']).path
+            agentdb, agentid = agenturi.strip(' /').split('/')
+            row['AgentURI']=url_for('agenttnt', id=agentid, _external=True)
+            agent = getAgent('DiversityAgents_TNT', agentid)
+            if len(agent) > 0:
+                row['AgentID'] = agent[0]['AgentID']
+                row['AgentTitle'] = agent[0]['AgentTitle']
+                row['GivenName'] = agent[0]['GivenName']
+                #row['GivenNamePostfix'] = agent[0]['GivenNamePostfix']
+                #row['InheritedNamePrefix'] = agent[0]['InheritedNamePrefix']
+                row['InheritedName'] = agent[0]['InheritedName']
+                #row['InheritedNamePostfix'] = agent[0]['InheritedNamePostfix']
+                row['Abbreviation'] = agent[0]['Abbreviation']
+                row['AgentType'] = agent[0]['AgentType']
+        taxonlist_eml = render_template("taxonlist_flat_eml.j2", database=database, id=id, agents=projectagentlist, project=project[0], thisurl = thisurl) 
+        
+        taxonlist_meta = render_template("meta.xml.j2", database=database, id=id, project=project[0], commonnamesexist=(len(taxoncommonnamelist) > 0) )
+        
         from flask import Flask, send_file
         from zipfile import ZipFile
         from StringIO import StringIO
@@ -78,16 +107,17 @@ class   darwin_core_zip( restful.Resource ):
         inMemoryOutputFile = StringIO()
 
         zipFile = ZipFile(inMemoryOutputFile, 'w') 
-        zipFile.writestr('eml.xml', 'hello world!')
-        zipFile.writestr('meta.xml', 'hello world, again!')
+        zipFile.writestr('eml.eml', taxonlist_eml.encode('utf-8'))
+        zipFile.writestr('meta.xml', taxonlist_meta.encode('utf-8'))
         zipFile.writestr('taxon.csv', taxon_csv.encode('utf-8'))
-        zipFile.writestr('common-names.csv', taxoncommonname_csv.encode('utf-8'))
+        if len(taxoncommonnamelist)>0:
+            zipFile.writestr('common-names.csv', taxoncommonname_csv.encode('utf-8'))
         zipFile.close()
 
         inMemoryOutputFile.seek(0)
 
         return send_file(inMemoryOutputFile,
-                     attachment_filename="".join(["DTNtaxonlist_", str(id) , "_" , timestr ,".zip"]),
+                     attachment_filename="".join(["DTNtaxonlist_",database, "_" , str(id) , "_" , timestr ,".zip"]),
                      mimetype =  'application/zip',
                      as_attachment=True)    
     
