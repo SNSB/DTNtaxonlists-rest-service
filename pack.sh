a8e8c6690742e2e8c6970b784d127c492976dbdf
stefan seifert <seifert@bsm.mwn.de>
2016-01-27 12:28:37 +0100

    new file static/revicison is automatically created from ./pack.sh and estimates the commit number (git log entries) modified pack.sh and makefilelist.sh to wirite filelist.txt and requirements.txt only if there are real changes (no touch only)

--- pack.sh
+++ pack.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-pip freeze > requirements.txt
+git diff-index --quiet HEAD && echo "$(git log | grep commit | wc -l)" > static/revision.txt || echo  "$(( $(git log | grep commit | wc -l) + 1 ))" > static/revision.txt
+pip freeze > requirements.tmp
+[[ $( diff requirements.txt requirements.tmp ) ]] && mv requirements.{tmp,txt}
+rm requirements.tmp
 ./makefilelist
+[[ $( diff filelist.txt filelist.tmp ) ]] && mv filelist.{tmp,txt}
+rm filelist.tmp
 rm restnames.tar
 tar -c -h -v -T filelist.txt -f restnames.tar
